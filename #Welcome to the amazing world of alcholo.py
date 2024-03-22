7de040f6681d971ed4dc69a354f752b4007d7fa7
gHill27 <142995904+gHill27@users.noreply.github.com>
2024-03-22 16:21:13 -0400

    Add files via upload

---
+++ #Welcome to the amazing world of alcholo.py
@@ -0,0 +1,56 @@
+#Welcome to the amazing world of alcholol. Take a seat and some burnetts. You're gonna need it to read this terrible code....
+import pygame
+import random
+import time
+
+"""SCREENS"""
+START = 1
+STATS = 2
+LEADERBOARD = 3
+SETTINGS = 4
+BREWING = 5
+current_screen = START
+
+
+
+
+
+
+pygame.init()
+SCREEN = pygame.display.set_mode((500,500), pygame.HWSURFACE)
+RUNNING = True
+while RUNNING:
+    SCREEN.fill((0,0,0))
+    SCREEN.fill
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            RUNNING = False
+    pygame.display.update()
+pygame.quit()
+
+
+def display_start():
+    pass
+
+
+while True: 
+    if current_screen == START:
+        display_start()
+
+
+
+
+
+
+class Bottle: 
+    def __init__(self, flavor, percentage, type, name):
+        self.flavor = flavor
+    def print_flavor(self):
+        print(self.flavor)
+
+
+class Pump:
+    def __init__(self,Port):
+        self.Port = Port
+    def start_pump(self, duration, ml):
+        pass
